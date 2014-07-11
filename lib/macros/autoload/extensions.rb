f083ce397b5ea4235b35d27a1db28c0419a8c736
Rachel Wall <solistra@gmx.com>
2014-07-10 20:28:15 -0400

    Provide YARD documentation and SES::Console.multiline method.

--- lib/macros/autoload/extensions.rb
+++ lib/macros/autoload/extensions.rb
@@ -5,44 +5,84 @@
 # Maker to allow easier use of the SES Console and provide general debugging
 # methods.
 #++
+
+# SES
 # =============================================================================
+# The top-level namespace for all SES scripts.
+module SES
+  # Console
+  # ===========================================================================
+  # Provides methods to facilitate an interactive Ruby console environment.
+  module Console
+    # Prompts related to multiline editing.
+    @prompt[:multiline] = '^  '
+    @prompt[:multi_end] = '<<'
+    
+    # Enables multiple lines of input. This method collects strings of user
+    # input until the input string matches the given `end_of_input` string (the
+    # `:multi_end` value of the `@prompt` hash by default) and returns the
+    # entirety of the collected input.
+    # 
+    # @param end_of_input [String] the end-of-input delimiter to use
+    # @return [String] the collected input
+    def self.multiline(end_of_input = @prompt[:multi_end])
+      script = ''
+      loop do
+        print(@prompt[:multiline])
+        break if (input = gets).chomp == end_of_input
+        script << input
+      end
+      script
+    end
+  end
+end
 # Object
 # =============================================================================
-# Superclass of all objects except BasicObject.
+# Superclass of all objects except `BasicObject`.
 class Object
   # Provides a sorted array of the instance methods defined on this object in
   # particular.
+  # 
+  # @return [Array<Symbol>] a sorted list of instance methods
   def instance_methods
     (methods - self.class.superclass.methods).sort!
   end
   
-  # Determines if the object is included in the given collection. Example:
+  # Determines if the object is included in the given collection.
   # 
-  #     1.in?([1, 2, 3]) # => true
+  # @example
+  #   1.in?([1, 2, 3]) # => true
   # 
+  # @param other [Object] the collection to test against
+  # @return [Boolean] `true` if the callee is in the given collection, `false`
+  #   otherwise
   def in?(other)
     other.include?(self) rescue false
   end
   
   # Returns the object itself.
+  # 
+  # @return [self]
   def itself
     self
   end
 end
-# =============================================================================
 # Enumerable
 # =============================================================================
-# Standard module included in collections -- provides well-known methods such
-# as `each` and `map`.
+# Standard module included in collections.
 module Enumerable
   # Recursively calls `Enumerable#map!` with the given arguments on the
   # collection in the given order, effectively chaining them together. Objects
   # with a defined `#call` method may be chained, as well; in this case, the
   # return value of `#call` is mapped to each object in the collection.
   # 
-  # Example:
-  #     [33, 50, 75].chain!(:to_f, ->(i) { i / 3 }, :round) # => [11, 17, 25]
+  # @example
+  #   [33, 50, 75].chain!(:to_f, ->(i) { i / 3 }, :round) # => [11, 17, 25]
   # 
+  # @param list [Array<Symbol, #call] list of methods and Proc objects to chain
+  # @return [Object] the result of the method chain
+  # 
+  # @see #chain
   def chain!(*list)
     return self if list.empty?
     map! do |item|
@@ -52,45 +92,37 @@ module Enumerable
   end
   
   # Copies the object this method is called upon via `dup` before calling
-  # `chain!`, ensuring non-destructive chaining.
+  # {#chain!}, ensuring non-destructive chaining.
+  # 
+  # @see #chain!
   def chain(*list)
     dup.chain!(*list)
   end
-  
-  # Returns the first item in the collection for which the passed block
-  # evaluates to a value other than `false` or `nil`. Returns `nil` if no match
-  # was found.
-  # 
-  # Example:
-  #     [1, 3, 5, 8, 10].find_first(&:even?) # => 8
-  # 
-  def find_first
-    each { |object| return object if yield object }
-    nil
-  end
 end
-# =============================================================================
 # String and Symbol
 # =============================================================================
 [String, Symbol].each do |base|
   # Provide the `~` unary operator for symbols and strings as a shortcut for
-  # running macro files in the SES Console. Example:
+  # running macro files in the SES Console.
   # 
-  #     ~:read_file # Calls SES::Console.macro(:read_file)
+  # @example
+  #   ~:read_file # Calls SES::Console.macro(:read_file)
   # 
   base.send(:define_method, :~) { SES::Console.macro(self.to_sym) }
 end
-# =============================================================================
 # String
 # =============================================================================
 # Mutable representation of a string of characters.
 class String
-  # Attempts to convert the string into a fully resolved constant. Returns
-  # `nil` if the constant could not be resolved. Examples:
+  # Attempts to convert the string into a fully resolved constant.
   # 
-  #     'SceneManager'.to_const # => SceneManager
-  #     'Console'.to_const(SES) # => SES::Console
+  # @example
+  #   'SceneManager'.to_const # => SceneManager
+  #   'Console'.to_const(SES) # => SES::Console
   # 
+  # @param base [Constant] the base constant to begin resolution with
+  # @return [Constant, nil] the fully resolved constant if found, `nil`
+  #   otherwise
   def to_const(base = Object)
     split('::').reduce(base) { |obj, const| obj.const_get(const) }
   rescue NameError
@@ -100,25 +132,39 @@ class String
   # Converts the string into a Proc by first converting it to a Symbol and
   # calling `#to_proc` on the result.
   # 
-  # Example:
-  #     string = 'to_f'
-  #     [1, 2, 3].map(&string) # => [1.0, 2.0, 3.0]
+  # @example
+  #   string = 'to_f'
+  #   [1, 2, 3].map(&string) # => [1.0, 2.0, 3.0]
   # 
+  # @return [Proc] the requested Proc object
   def to_proc
     to_sym.to_proc
   end
 end
-# =============================================================================
 # Kernel
 # =============================================================================
 # Methods defined here are automatically available to all Ruby objects.
 module Kernel
-  # Delegators for the `bind`, `rebind`, `macro`, and `multiline` methods of
-  # the SES Console, allowing these methods to be used within any context.
-  def bind(object, &block) SES::Console.bind(object, &block) end
-  def rebind(&block)       SES::Console.rebind(&block)       end
-  def macro(id)            SES::Console.macro(id)            end
+  # Delegator for the {SES::Console.bind} method.
+  # @see SES::Console.bind
+  def bind(object, &block)
+    SES::Console.bind(object, &block)
+  end
   
+  # Delegator for the {SES::Console.rebind} method.
+  # @see SES::Console.rebind
+  def rebind(&block)
+    SES::Console.rebind(&block)
+  end
+  
+  # Delegator for the {SES::Console.macro} method.
+  # @see SES::Console.macro
+  def macro(id)
+    SES::Console.macro(id)
+  end
+  
+  # Delegator for the {SES::Console.multiline} method.
+  # @see SES::Console.multiline
   def multiline(end_of_input = SES::Console.prompt[:multi_end])
     SES::Console.multiline(end_of_input)
   end
@@ -126,12 +172,14 @@ module Kernel
   # Closes an open console session by raising a `SystemExit` exception. This is
   # particularly useful when the context is bound to an object which has its
   # own `exit` method defined (such as `SceneManager`).
+  # 
+  # @raise [SystemExit] if the console is enabled
+  # @return [nil] if the console is disabled
   def close
-    raise(SystemExit) if SES::Console.enabled
+    raise SystemExit if SES::Console.enabled
   end
 end
-# =============================================================================
 # Main
 # =============================================================================
-# Link the Console constant to SES::Console.
+# Link to SES::Console.
 Console = SES::Console
