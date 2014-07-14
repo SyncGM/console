2e8d9a0257393d40c70adf300565ef4af0fc6693
Rachel Wall <solistra@gmx.com>
2014-07-14 16:28:58 -0400

    Simplify `kill_events` and `restore_events` macro files.

--- lib/macros/utility/kill_events.rb
+++ lib/macros/utility/kill_events.rb
@@ -17,14 +17,10 @@ module SES::Console::Macros
     # @note This method may be called when the scene is not an instance of
     #   `Scene_Map` and will still perform the appropriate action.
     # 
-    # @return [Hash{FixNum => Game_Event}, nil] hash of events on the current
-    #   map if any events were killed, `nil` otherwise
+    # @return [Hash{FixNum => Game_Event}] hash of events on the current map
     def self.kill_events
-      map_events = $game_map.instance_variable_get(:@map).events
-      return nil if @events_killed == map_events
       $game_map.events.clear
       SceneManager.call(Scene_Map) if SceneManager.scene_is?(Scene_Map)
-      @events_killed = map_events
       $game_map.events
     end
   end
--- lib/macros/utility/restore_events.rb
+++ lib/macros/utility/restore_events.rb
@@ -12,19 +12,15 @@ module SES::Console::Macros
   # ===========================================================================
   # Provides macros which perform various utility operations.
   module Utility
-    # Restores all of the killed events on the current map.
+    # Restores all of the events on the current map.
     # 
     # @note This method may be called when the scene is not an instance of
     #   `Scene_Map` and will still perform the appropriate action.
     # 
-    # @return [Hash{FixNum => Game_Event}, nil] hash of events on the current
-    #   map if events were restored, `nil` otherwise
+    # @return [Hash{FixNum => Game_Event}] hash of events on the current map
     def self.restore_events
-      map_events = $game_map.instance_variable_get(:@map).events
-      return nil unless @events_killed == map_events
       $game_map.setup_events
       SceneManager.call(Scene_Map) if SceneManager.scene_is?(Scene_Map)
-      @events_killed = nil
       $game_map.events
     end
   end
